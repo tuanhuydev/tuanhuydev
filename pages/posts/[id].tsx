9884a852800389975ae73aa4c96382f7f726d7d1
tuanhuydev <tuanhuydev@gmail.com>
2023-02-05 20:08:12 +0700

    feat(styles): setup tailwind

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Post() {
+  return (
+    <div>
+    <Head>
+        <title>@tuanhuydev - Post title</title>
+    </Head>
+    <main>
+       Single Post
+    </main>
+</div>
+  )
+}
