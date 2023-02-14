789aabbaacb79925656a9e83a6c5043785f119e4
tuanhuydev <tuanhuydev@gmail.com>
2023-02-14 16:34:59 +0700

    feat(home): hero section

--- shared/utils/dom.ts
+++ shared/utils/dom.ts
@@ -1,8 +1,21 @@
+import { STORAGE_KEY, DEFAULT_THEME } from "@shared/configs/constants";
+
 export const reflectTheme = (theme: string) => {
   const htmlElement = document.querySelector("html");
   if (htmlElement) {
     htmlElement.setAttribute("class", theme);
+    document.documentElement.setAttribute("data-theme", theme);
   }
-  document.firstElementChild?.setAttribute("data-theme", theme);
-  
+};
+
+export const updateLocalStorage = (key: string, value: any) => {
+  localStorage.setItem(key, value);
+};
+
+export const getThemeValue = (): string => {
+  if (localStorage.getItem(STORAGE_KEY))
+    return localStorage.getItem(STORAGE_KEY) || DEFAULT_THEME;
+  return window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : DEFAULT_THEME;
 };
