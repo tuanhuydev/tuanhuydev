2d78482f9f1f7c72168dd753190ea4d02e18a72a
tuanhuydev <tuanhuydev@gmail.com>
2023-03-09 16:10:28 +0700

    refactor(ci): commitlint check on push

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "header-max-length": [2, "always", 72],
+        "body-case": [2, "always", ["lower-case"]],
+        "header-case": [2, "always", ["lower-case"]],
+        "scope-case": [2, "always", ["lower-case"]],
+        "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "revert", "chore", "build", "release"]],
+    }
+}
