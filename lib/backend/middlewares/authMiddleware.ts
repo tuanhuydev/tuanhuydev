3f9c1b91e8a1f8c417bf43cc0a8c6bc74206a939
tuanhuydev <tuanhuydev@gmail.com>
2023-09-21 21:22:51 +0700

    feat(be): users apis

--- lib/backend/middlewares/authMiddleware.ts
+++ lib/backend/middlewares/authMiddleware.ts
@@ -13,19 +13,17 @@ const withAuthMiddleware = (handler: Function) => async (req: NextRequest, param
 	const network = Network(req);
 	try {
 		const authorization: string | null = headers().get('authorization');
-		if (authorization) {
-			const accessToken = extractTokenFromRequest(authorization);
-			const secret = jwt.verify(accessToken, ACCESS_TOKEN_SECRET);
-			const currentTimestamp = Math.floor(Date.now() / 1000);
+		if (!authorization) throw new UnauthorizedError('Token not found');
 
-			if (!secret || typeof secret !== 'object' || (secret?.exp as number) < currentTimestamp || !secret.userId) {
-				throw new UnauthorizedError('Token expired');
-			}
-			return await handler(req, {
-				...params,
-				userId: secret.userId,
-			});
-		}
+		const accessToken = extractTokenFromRequest(authorization);
+		const secret = jwt.verify(accessToken, ACCESS_TOKEN_SECRET);
+		const currentTimestamp = Math.floor(Date.now() / 1000);
+
+		const invalidSecret = !secret || typeof secret !== 'object' || !secret.userId;
+		const isTokenExpired = invalidSecret || (secret?.exp as number) < currentTimestamp;
+		if (isTokenExpired) throw new UnauthorizedError('Token expired');
+
+		return await handler(req, { ...params, userId: secret.userId });
 	} catch (error) {
 		const isBaseError = error instanceof BaseError;
 		return network.failResponse(isBaseError ? error : new BaseError((error as Error).message));
