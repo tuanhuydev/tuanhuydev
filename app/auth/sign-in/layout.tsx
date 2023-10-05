6bc28b79d56ba49939ef2de4067f99616efdf876
tuanhuydev <tuanhuydev@gmail.com>
2023-10-05 11:41:23 +0700

    fix(fe): home page posts unable to load

--- app/auth/sign-in/layout.tsx
+++ app/auth/sign-in/layout.tsx
@@ -1,14 +1,12 @@
 'use client';
 
-import i18nextConfig from '@lib/configs/next-i18next.config';
 import store from 'lib/store';
 import { PropsWithChildren } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 
 export default function SignInLayout({ children }: PropsWithChildren) {
-	const currentLocale = i18nextConfig.i18n.defaultLocale;
 	return (
-		<html lang={currentLocale}>
+		<html lang="en">
 			<body>
 				<ReduxProvider store={store}>{children}</ReduxProvider>
 			</body>
