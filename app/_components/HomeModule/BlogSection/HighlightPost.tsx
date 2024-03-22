747f4e0ea956528833189982506e51df7a3eb752
tuanhuydev <tuanhuydev@gmail.com>
2024-03-22 14:06:15 +0700

    fix(google): marketing tag

--- app/_components/HomeModule/BlogSection/HighlightPost.tsx
+++ app/_components/HomeModule/BlogSection/HighlightPost.tsx
@@ -15,11 +15,17 @@ export default function HighlightPost({ post, className }: HighlightPostProps) {
   return (
     <a
       href={`/posts/${post.slug}`}
-      className={`col-span-full md:col-span-1 lg:col-span-2 rounded-md transition-all drop-shadow duration-150 hover:drop-shadow-xl hover:scale-105 ease-in-out cursor-pointer ${className}`}>
+      className={`col-span-full md:col-span-1 lg:col-span-2 rounded-md transition-all drop-shadow duration-150 hover:drop-shadow-xl ease-in-out cursor-pointer ${className}`}>
       <div className="z-0 h-full flex flex-col rounded-md relative bg-white dark:bg-slate-800 dark:border dark:border-slate-700 px-5 py-3">
         {thumbnail && (
-          <div className="w-full h-72 lg:h-72 grow relative mb-2">
-            <Image src={thumbnail} className="object-cover rounded-md" alt={title} fill sizes="50vw" />
+          <div className="w-full h-72 lg:h-72 grow relative mb-2 overflow-hidden">
+            <Image
+              src={thumbnail}
+              className="object-cover transition-all duration-500 rounded-md hover:scale-105"
+              alt={title}
+              fill
+              sizes="50vw"
+            />
           </div>
         )}
         <div className="mt-auto">
