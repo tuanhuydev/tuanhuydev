dfe5d1b1cbe1119f94b13b9bf244cee6db7ab829
tuanhuydev <tuanhuydev@gmail.com>
2024-08-25 17:12:50 +0700

    feat(sprint): manage sprint card

--- app/components/DashboardModule/Sidebar/Item.tsx
+++ app/components/DashboardModule/Sidebar/Item.tsx
@@ -1,6 +1,7 @@
-import { RootState } from "@lib/configs/types";
+import { Type } from "@aws-sdk/client-s3";
 import { UserPermissions } from "@lib/shared/commons/constants/permissions";
 import { isPathActive } from "@lib/shared/utils/helper";
+import { Typography } from "@mui/material";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -27,10 +28,12 @@ export default function Item({ label, icon, path, id }: ItemProps) {
     () => (
       <Link href={path} key={path} prefetch={false} className={id === UserPermissions.VIEW_SETTING ? "mt-auto" : ""}>
         <li
-          className={`ease-in duration-200 rounded-sm mb-1 dark:text-slate-300  cursor-pointer py-2 px-3 hover:bg-primary hover:text-slate-50 dark:hover:bg-slate-600 dark:hover:text-slate-50 ${activeClass}`}>
+          className={`ease-in duration-200 font-sans rounded-sm mb-1 dark:text-slate-300  cursor-pointer py-2 px-3 hover:bg-primary hover:text-slate-50 dark:hover:bg-slate-600 dark:hover:text-slate-50 ${activeClass}`}>
           <div className="capitalize flex items-center relative min-w-0">
             <span className="mr-3 leading-none align-middle">{icon}</span>
-            <span className="truncate">{label}</span>
+            <Typography variant="body2" className="truncate">
+              {label}
+            </Typography>
           </div>
         </li>
       </Link>
