a01586e8df39a9b77adb8304bc1772ef9118fd1b
tuanhuydev <tuanhuydev@gmail.com>
2025-06-02 23:12:01 +0700

    fix(project): tasks and sub-tasks are not sync

--- app/dashboard/home/page.tsx
+++ app/dashboard/home/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import PageContainer from "@app/components/DashboardModule/PageContainer";
 import PostWidget from "@app/components/DashboardModule/widgets/PostWidget";
 import ProjectWidget from "@app/components/DashboardModule/widgets/ProjectWidget";
 import TodayTaskWidget from "@app/components/DashboardModule/widgets/TodayTaskWidget";
@@ -9,12 +10,14 @@ import { Suspense } from "react";
 export default function Page() {
   const userResources = new Set<string>();
   return (
-    <div className="flex flex-wrap gap-4">
-      <Suspense fallback={<>Loading...</>}>
-        <TodayTaskWidget />
-      </Suspense>
-      {userResources.has(UserPermissions.VIEW_PROJECT) && <ProjectWidget />}
-      {userResources.has(UserPermissions.VIEW_POST) && <PostWidget />}
-    </div>
+    <PageContainer title="Home">
+      <div className="flex flex-wrap gap-4">
+        <Suspense fallback={<>Loading...</>}>
+          <TodayTaskWidget />
+        </Suspense>
+        {userResources.has(UserPermissions.VIEW_PROJECT) && <ProjectWidget />}
+        {userResources.has(UserPermissions.VIEW_POST) && <PostWidget />}
+      </div>
+    </PageContainer>
   );
 }
